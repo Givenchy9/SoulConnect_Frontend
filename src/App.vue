d7d5e923cdf56d1240a2b6d37fec88082a876c0c
Mohamadka1 <124268603+Mohamadka1@users.noreply.github.com>
2025-01-23 09:22:43 +0100

    remove header from some pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <Header />
+  <Header v-if="showHeader" /> 
   <RouterView />
-
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
 import Header from '@/components/Header.vue'
 
+
+const route = useRoute()
+
+const showHeader = computed(() => {
+  return !['login', 'register', 'home', 'settings', 'dashboard'  ].includes(route.name as string)
+})
 </script>
